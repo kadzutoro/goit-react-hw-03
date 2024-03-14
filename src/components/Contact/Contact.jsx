bdb185d7403a3bb6fae5418e1461e46534c9cf56
kadzutoro <nikitashpir132@gmail.com>
2024-03-14 23:48:50 +0200

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,16 @@
 import css from './Contact.module.css'
+import { IoPerson } from 'react-icons/io5';
+import { FaPhoneAlt } from 'react-icons/fa';
 export default function Contact({data: {id, name, number}, onDelete}) {
     return (
         <div className={css.contact}>
             <div className={css.info}>
                 <div className={css.name}>
+                <IoPerson />
             <p>{name}</p>
             </div>
             <div className={css.number}>
+            <FaPhoneAlt />
             <p>{number}</p>
             </div>
             </div>
